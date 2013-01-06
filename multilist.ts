17626aa5fd90060b8ec46e11b90083f132f84c6c
Mark Soper <masoper@gmail.com>
2013-01-06 15:40:25 -0500

    adding new app called multilist alongside original listmove -- demonstrates that an arbitrary number of lists are supported by the list.List class

---
+++ multilist.ts
@@ -0,0 +1,42 @@
+
+//
+// Typescript example project
+// Mark Soper
+//
+
+//
+// reference the list module that defines the List class
+//
+///<reference path='list.ts'/>
+
+//
+// to be run after DOM is loaded
+//
+var main = function() {
+  var initialData = {
+    left: ["horse", "cat", "dog"],
+    right: ["mouse", "bird", "squirrel"],
+    leftLower: ["zebra", "oryx", "gazelle"],
+    rightLower: ["eagle", "bear", "orca"]
+  };
+  var lists = {};
+  for (var listName in initialData) {
+    lists[listName] = new list.List(
+      listName,
+      document.getElementById(listName),
+      initialData[listName]
+    );
+    lists[listName].render();
+  }
+};
+
+//
+// use window.onload in favor of window.addEventListener
+// because cross browser support more important than being able to 
+// attach multiple event handlers
+//
+window.onload = main;
+
+
+
+
